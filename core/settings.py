4249be6d7e641d5e3313769e07f3d1680482e916
moh-x <mo.xodeeq@gmail.com>
2020-08-27 23:18:51 +0100

    initial commits

--- core/settings.py
+++ core/settings.py
@@ -1,12 +1,13 @@
 
-import django_heroku
 from pathlib import Path
 import os
 import environ
+import django_heroku
+
+
 env = environ.Env()
 # reading .env file
 environ.Env.read_env()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -36,7 +37,6 @@ INSTALLED_APPS = [
 
     'debug_toolbar',
     'django_extensions',
-    'django_heroku',
 
     'users',
 ]
