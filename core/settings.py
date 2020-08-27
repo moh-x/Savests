7c7df224f8bb364bf6831231d4c496c4363ec8bd
moh-x <mo.xodeeq@gmail.com>
2020-08-27 23:39:49 +0100

    initial commits

--- core/settings.py
+++ core/settings.py
@@ -125,10 +125,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_root')
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 
 # debug-toolbar settings
